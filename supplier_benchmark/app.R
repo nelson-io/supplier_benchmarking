f1090e5f3dca6b676b32fc3738c8d39b4d68ac8a
Nelson Shilman <nelsonshilman@gmail.com>
2020-05-30 21:30:37 -0300

    make Standardized calculations & update plots

--- supplier_benchmark/app.R
+++ supplier_benchmark/app.R
@@ -5,18 +5,19 @@ library(janitor)
 library(ggplot2)
 library(rio)
 library(purrr)
+library(stringr)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Supplier Benchmarking"),
+    titlePanel("Supplier Benchmarking with standardized scores"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
             fileInput("df",
-                      "Choose your file"),
+                      "Choose your file (.xlsx, .xls or .csv)"),
             uiOutput("sliders")
             
             # sliderInput("bins",
@@ -29,8 +30,13 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           tableOutput("table_1", width = "50%"),
-           plotOutput("plot_1", width = "50%")
+            tabsetPanel(
+                tabPanel("Score", plotOutput("plot_1"),
+                         textOutput("subtitle"),tableOutput("test")),
+                tabPanel("Original table", dataTableOutput("table_2")),
+                
+                tabPanel("Standardized table", dataTableOutput("table_1"))
+            )
         )
     )
 )
@@ -47,44 +53,83 @@ server <- function(input, output) {
         import(input$df$datapath)
     })
     
+    data_standardized <- reactive({
+        data() %>% 
+            mutate_all(~(.-mean(.))/sd(.))
+    })
+    
+    datanames <- reactive({
+        names(data())
+    })
+    
+    #set weights
+    
+    rel_imp <- reactive({
+       
+       map_dbl(names(data()), ~ input[[paste0("var",.)]])
+        # map_dbl(paste0("input$var",names(data())), get) 
+    })
+    
+    weights <- reactive({
+      
+          rel_imp()/sum(rel_imp())
+    })
+    
     #Set Scores
     scores_vect <- reactive({
-        map2_df(data(), map_dbl(data(),mean), ~ .x/.y) %>% 
-            transmute(scores = rowSums(.)) %>% pull()
+        req(weights())
+        map2_df(data_standardized(), weights(), ~ (.x*.y)) %>% 
+            transmute(scores = rowSums(.)) %>% pull() 
     })
     
     #Bind Scores
     data_scored <- reactive({
-        cbind(Provider = seq_along(scores_vect()), data(), Score = scores_vect())
+        cbind(Provider = seq_along(scores_vect()), data_standardized(), Score = scores_vect())
     })
+    
+    
+        
         
     #Generate sliders
     output$sliders <- renderUI({
         pvars <- names(data())
         lapply(seq(pvars), function(i) {
-            sliderInput(inputId = paste0("range", pvars[i]),
-                        label = paste0("Weight of: ", pvars[i]),
-                        min = 1, max = 100, value = 100)
+            sliderInput(inputId = paste0("var", pvars[i]),
+                        label = paste0("Relative importance of:", pvars[i]),
+                        min = 0, max = 1, value = 1)
         })
-        
+
     })
         
 
     #Render table
-    output$table_1 <- renderTable({
-       data_scored()
+    output$table_1 <- renderDataTable({
+       data_scored() %>% mutate_at(vars(-Provider),~round(.,4))
     })
     
     output$plot_1 <- renderPlot({
         ggplot(data_scored())+
             geom_col(aes(x = as.factor(Provider), y = Score), fill = 'steelblue')+
             theme_bw()+
-            ggtitle("Score by provider")+
+            ggtitle("Standardized score by provider")+
             xlab("Provider")+
             ylab("Score")
     })
-
     
+    output$test <- renderTable({
+        table_2 <- weights() %>% t() %>% data.frame() 
+        names(table_2) <- datanames()
+        table_2
+    })
+    
+    output$subtitle <- renderText({
+        req(weights())
+        "Weights"
+    })
+
+    output$table_2 <- renderDataTable({
+        data()
+    })
 
 
     
